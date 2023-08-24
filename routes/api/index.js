d199c71d5e02eafaa681e3c85a57e693029e6df8
Cameron French <cameron.french92@gmail.com>
2023-08-23 23:38:48 -0400

    updated api index.js to include thoughts route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const thoughtsRoutes = reuire('./thoughtsRoutes');
 
 router.use('/users', userRoutes);
+router.use('/thoughts', thoughtsRoutes);
+
 
 module.exports = router;
